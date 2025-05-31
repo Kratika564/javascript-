5f6014d3aa863b3983c3cf28e1f1d0f0aed435f1
Kratika564 <kratikaagrawal176@gmail.com>
2025-05-31 21:18:39 +0530

    Add files via upload

---
+++ data_conversion.js
@@ -0,0 +1,40 @@
+//conver to number
+
+// let score=undefined
+// console.log(score)
+// console.log(typeof score);
+// let value=Number(score);
+// console.log(value)
+// console.log(typeof value);
+ //"33"=>33
+ //"33abc"=>NaN(not a number but type is a number )
+ //true=>1
+ //null=>0
+//undefined=>nan 
+
+//convert to boolean
+let score
+console.log(score)
+console.log(typeof score);
+let valueboolean=Boolean(score);
+console.log(valueboolean)
+console.log(typeof valueboolean);
+//"33"=>true
+ //"33abc"=>true
+ //null=>false
+//undefined=>false 
+//""=>false
+
+//convert to string
+// let val
+// console.log(val)
+// console.log(typeof val);
+// let valuestring=String(val);
+// console.log(valuestring)
+// console.log(typeof valuestring);
+
+
+//33=>"33"
+//true=>"true"
+//null=>"null"
+//undefined=undefined
